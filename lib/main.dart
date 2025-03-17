568cbadb6873ccd2ba5e94e0eb2d28ef4042ad76
sstTrainee <ssttrainees@gmail.com>
2025-03-17 14:55:42 +0530

    login module done

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'github demo home page'),
+      home: const MyHomePage(title: 'login module'),
     );
   }
 }
